3400692064f15041c0f93cd82b660eaae8f14c42
Sakib Al Islam <73927803+sakib-al@users.noreply.github.com>
2023-02-16 17:32:50 +0600

    update

--- app/Http/Controllers/Api/QuizController.php
+++ app/Http/Controllers/Api/QuizController.php
@@ -4,8 +4,47 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class QuizController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+
+    public function store(Request $request)
+    {
+        // return $request;
+
+        try {
+
+            $validator = Validator::make($request->data, [
+                'title' => 'required',
+                'questions.*.title' => 'required',
+                'questions.*.answers.*.title' => 'required'
+            ],[
+                'title.required' => 'Quiz Title is Required',
+                'questions.*.title.required' => 'Question Title is Required',
+                'questions.*.answers.*.title.required' => 'Question Answer Title is Required'
+            ]);
+
+            if ($validator->fails())
+                return response()->json($validator->errors(), 500);
+
+
+        } catch (\Throwable $th) {
+
+
+        }
+    }
+
+
+    public function update(Request $request,$id)
+    {
+
+    }
+
+
 }
